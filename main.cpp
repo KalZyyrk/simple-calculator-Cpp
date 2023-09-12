6ffae21eeb0f4eadab1b7424352bf15937bd1ca4
Ulysse <ulysse.hischke.dev@gmail.com>
2023-09-12 10:19:55 +0200

    modify: create the logic of the calculator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    char op;
+    double num1;
+    double num2;
+    double result;
+    bool valid = false;
+
+
+    while (!valid) {
+        std::cout << "Your Calculator !\nEnter your Operation -> ";
+        std::cin >> num1 >> op >> num2;
+
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                valid = true;
+                break;
+            case '-':
+                result = num1 - num2;
+                valid = true;
+                break;
+            case '*':
+                result = num1 * num2;
+                valid = true;
+                break;
+            case '/':
+                result = num1 / num2;
+                valid = true;
+                break;
+            default:
+                std::cout << "Invalid Operator\nValid operator are + - * / \n";
+        }
+    }
+
+    std::cout << "the result: " << result;
     return 0;
 }
