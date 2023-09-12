5eddc25739af8260e74dac22f09d18306794b34b
Ulysse <ulysse.hischke.dev@gmail.com>
2023-09-12 10:25:06 +0200

    add: comment to main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 
 int main() {
 
-    char op;
-    double num1;
+    // declare all the variable needed
+    char op;                // char op is for the operator, the operator will be a character like '+, -, *, /'
+    double num1;            // double is to provided decimal if needed
     double num2;
     double result;
     bool valid = false;
@@ -11,8 +12,10 @@ int main() {
 
     while (!valid) {
         std::cout << "Your Calculator !\nEnter your Operation -> ";
+        // collect the input for the user
         std::cin >> num1 >> op >> num2;
 
+        // switch that make the operation based on the operator write by the user
         switch (op) {
             case '+':
                 result = num1 + num2;
@@ -31,10 +34,12 @@ int main() {
                 valid = true;
                 break;
             default:
+                // error Handler
                 std::cout << "Invalid Operator\nValid operator are + - * / \n";
         }
     }
 
+    // display the result
     std::cout << "the result: " << result;
     return 0;
 }
